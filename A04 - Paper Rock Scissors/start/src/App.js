e97694de206ed7f3ec806d036f6146c2809a8354
Pervez Iqbal <pervezfunctor@gmail.com>
2020-11-10 15:44:44 +0530

    add rock-paper-scissors

--- A04 - Paper Rock Scissors/start/src/App.js
+++ A04 - Paper Rock Scissors/start/src/App.js
@@ -4,7 +4,54 @@ import Paper from './icons/Paper';
 import Scissors from './icons/Scissors';
 import './App.css';
 
+const choices = [
+  { id: 1, name: 'rock', component: Rock, loosesTo: 2 },
+  { id: 2, name: 'paper', component: Paper, loosesTo: 3 },
+  { id: 3, name: 'scisscors', component: Scissors, loosesTo: 1 },
+];
+
 export default function App() {
+  const [userChoice, setUserChoice] = React.useState(null);
+  const [computerChoice, setComputerChoice] = React.useState(null);
+  const [wins, setWins] = React.useState(0);
+  const [losses, setLooses] = React.useState(0);
+  const [gameState, setGameState] = React.useState(null);
+
+  const handleUserChoice = (choice) => {
+    const choosen = choices.find((c) => c.id === choice);
+    setUserChoice(choosen);
+
+    if (
+      (choosen.name === 'rock' && computerChoice.name === 'scisscors') ||
+      (choosen.name === 'scisscors' && computerChoice.name === 'paper') ||
+      (choosen.name === 'paper' && computerChoice.name === 'rock')
+    ) {
+      setGameState('you win');
+      setWins((wins) => wins + 1);
+    } else if (choosen.id === computerChoice.id) {
+      setGameState('draw');
+    } else {
+      setGameState('you lose');
+      setLooses((looses) => looses + 1);
+    }
+  };
+
+  React.useEffect(() => {
+    const computerChoosen = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(computerChoosen);
+  }, []);
+
+  function renderComponent(choice) {
+    const SelectedComponent = choice.component;
+    return <SelectedComponent />;
+  }
+  function resetGame() {
+    setUserChoice(null);
+    setGameState(null);
+    const computerChoosen = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(computerChoosen);
+  }
+
   return (
     <div className="app">
       {/* information goes here */}
@@ -14,35 +61,46 @@ export default function App() {
         {/* wins vs losses stats */}
         <div className="wins-losses">
           <div className="wins">
-            <span className="number">0</span>
-            <span className="text">Wins</span>
+            <span className="number">{wins}</span>
+            <span className="text">{wins === 1 ? 'Win' : 'Wins'}</span>
           </div>
 
           <div className="losses">
-            <span className="number">0</span>
-            <span className="text">Losses</span>
+            <span className="number">{losses}</span>
+            <span className="text">{losses === 1 ? 'Lose' : 'looses'}</span>
           </div>
         </div>
       </div>
 
       {/* the popup to show win/loss/draw */}
-      {/* <div className="game-state"></div> */}
+      {gameState && (
+        <div className={`game-state ${gameState}`}>
+          <div>
+            <div className="game-state-content">
+              <p>{renderComponent(userChoice)}</p>
+              <p>{gameState}</p>
+              <p>{renderComponent(computerChoice)}</p>
+            </div>
+            <button onClick={() => resetGame()}>Play Again</button>
+          </div>
+        </div>
+      )}
 
       <div className="choices">
         {/* choices captions */}
-        <div>You</div>
+        <div>you</div>
         <div />
-        <div>Computer</div>
+        <div>computer</div>
 
         {/* buttons for my choice */}
         <div>
-          <button className="rock">
+          <button className="rock" onClick={() => handleUserChoice(1)}>
             <Rock />
           </button>
-          <button className="paper">
+          <button className="paper" onClick={() => handleUserChoice(2)}>
             <Paper />
           </button>
-          <button className="scissors">
+          <button className="scissors" onClick={() => handleUserChoice(3)}>
             <Scissors />
           </button>
         </div>
