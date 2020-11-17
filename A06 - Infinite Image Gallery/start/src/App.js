57e7458d85c8354b5d12a9f7214aa930e374968e
Pervez Iqbal <pervezfunctor@gmail.com>
2020-11-17 19:10:56 +0530

    add  changes to inifinite gallery

--- A06 - Infinite Image Gallery/start/src/App.js
+++ A06 - Infinite Image Gallery/start/src/App.js
@@ -6,46 +6,42 @@ import { SearchForm } from './SearchForm';
 
 const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
+export function usePrevious(value) {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+async function fetchImages(page, query) {
+  const baseUrl =
+    query && query.length !== 0
+      ? `https://api.unsplash.com/search/photos/?client_id=${accessKey}&page=${page}&query=${query}`
+      : `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}`;
+
+  let data = await (await fetch(baseUrl)).json();
+  return query ? data.results : data;
+}
+
 export default function App() {
   const [images, setImages] = React.useState([]);
   const [page, setPage] = React.useState(1);
   const [query, setQuery] = React.useState('');
-
-  const fetchData = () => {
-    fetch(
-      `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}`
-    )
-      .then((data) => data.json())
-      .then((data) => {
-        setImages([...images, ...data]);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const searchPhotos = (query) => {
-    return fetch(
-      `https://api.unsplash.com/search/photos/?client_id=${accessKey}&page=${page}&query=${query}`
-    )
-      .then((data) => data.json())
-      .then((data) => {
-        setImages([...data.results, ...data.results]);
-        setPage(page + 1);
-        console.log(page);
-      })
-      .catch(console.error);
-  };
+  // const prevQuery = React.usePrevious(query);
 
   const handleSearchForm = (text) => {
     setQuery(text);
+    setImages([]);
   };
 
+  // React.useEffect(() => setImages([]), [query]);
+
   React.useEffect(() => {
-    if (query) {
-      searchPhotos(query);
-    } else {
-      fetchData();
-    }
-  }, [query]);
+    fetchImages(page, query)
+      .then((data) => setImages((images) => [...images, ...data]))
+      .catch(console.error);
+  }, [page, query]);
 
   if (!accessKey) {
     return (
@@ -63,7 +59,6 @@ export default function App() {
         dataLength={images.length}
         next={() => {
           setPage((page) => page + 1);
-          fetchData();
         }}
         hasMore={true}
         loader={<h4>Loading...</h4>}
