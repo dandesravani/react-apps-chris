96ccb2cccebfd09ce42fb5e173006b252d54eb3b
Pervez Iqbal <pervezfunctor@gmail.com>
2020-12-11 15:48:51 +0530

    add Search with Algolia

--- B04 - Drag and Drop Math Card/start/src/App.js
+++ B04 - Drag and Drop Math Card/start/src/App.js
@@ -1,36 +1,90 @@
 import React from 'react';
 import './App.css';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+export const Number = ({ text }) => {
+  const [{ opacity }, drag] = useDrag({
+    item: { type: 'number', number: text },
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+  return (
+    <div className="card" ref={drag} style={{ opacity }}>
+      {text}
+    </div>
+  );
+};
+
+export const Operator = ({ text }) => {
+  const [{ opacity }, drag] = useDrag({
+    item: { type: 'operator', operator: text },
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+
+  return (
+    <div className="card" ref={drag} style={{ opacity }}>
+      {text}
+    </div>
+  );
+};
+
+export const Spot = ({ type, children, text }) => {
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: type,
+    drop: (item) => {},
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  let backgroundColor = '#f2f2f2';
+  if (canDrop) backgroundColor = '#3db897';
+  if (isOver) backgroundColor = '#4bdcb5';
+
+  return (
+    <div className="spot" ref={drop} style={{ backgroundColor }}>
+      {text}
+    </div>
+  );
+};
 
 export default function App() {
+  const [num1, setNum1] = React.useState(1);
+  const [num2, setNum2] = React.useState(1);
+  const [operator, setOperator] = React.useState('+');
+
   return (
-    <div className="app">
-      {/* math card */}
-      <div className="math-card">
-        <div className="spot">1</div>
-        <div className="spot">1</div>
-        <div className="spot">+</div>
-        <div className="total">2</div>
-      </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="app">
+        {/* math card */}
+        <div className="math-card">
+          <Spot type="number" text={num1} />
+          <Spot type="number" text={num2} />
+          <Spot type="operator" text={operator} />
+          <div className="total">{eval(`${num1} ${operator} ${num2}`)}</div>
+        </div>
 
-      <div>
-        <div className="cards numbers">
-          {Array(10)
-            .fill(0)
-            .map((n, i) => (
-              <div key={i} className="card">
-                {i + 1}
-              </div>
+        <div>
+          <div className="cards numbers">
+            {Array(10)
+              .fill(0)
+              .map((n, i) => (
+                <Number key={i} text={i} />
+              ))}
+          </div>
+
+          <div className="cards operators">
+            {['*', '-', '+', '/'].map((o, i) => (
+              <Operator text={o} key={i} />
             ))}
-        </div>
-
-        <div className="cards operators">
-          {['*', '-', '+', '/'].map((o, i) => (
-            <div key={i} className="card">
-              {o}
-            </div>
-          ))}
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
--- B06 - Real-Time Search/start/src/App.js
+++ B06 - Real-Time Search/start/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
 import './App.css';
+import Pagination from 'react-instantsearch-dom/dist/cjs/widgets/Pagination';
 
 // ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
 // demo data: https://www.algolia.com/doc/guides/building-search-ui/resources/demos/react/#media
@@ -10,10 +13,39 @@ import './App.css';
 // api key: ae8a3f7cff93d40282a4831930bfa38b
 // index name: products
 
+// app id:  GTT3RCZ7UP
+// api key:: d6b0eaa7d4f35635e8b9404f6a5f17e9
+// index name:: ecommerce
+
+const searchClient = algoliasearch(
+  'GTT3RCZ7UP',
+  'd6b0eaa7d4f35635e8b9404f6a5f17e9'
+);
+
+export const Product = ({ hit }) => {
+  return (
+    <a className="product" href={hit.url}>
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>${hit.price}</p>
+      </div>
+    </a>
+  );
+};
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="ecommerce">
+      <div className="app">
+        <div className="search-container">
+          <Stats />
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
   );
 }
